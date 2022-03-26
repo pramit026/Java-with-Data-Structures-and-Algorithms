6bb62a6c40f276d0161f5e4afd48b01b817e1bf9
Pramit Sahoo <pramitsahoo.gnipst@gmail.com>
2022-03-26 21:02:36 +0530

    Add files via upload

---
+++ 01.First-Java-Program/first-idea-program/Basics.java
@@ -0,0 +1,24 @@
+package com.Pramit;
+
+public class Basics {
+    public static void main(String[] args) {
+//        int a = 10;
+// if else
+//        if(a == 10){
+//            System.out.println("Hello world");
+//        }
+
+//        // while loop
+//        int count = 1;
+//        while(count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+        for (int count = 1; count < 5; count++) {
+            System.out.println(count);
+        }
+        // when you dont know for how many times loop will run , use while loop.when you know, use for
+    }
+}
