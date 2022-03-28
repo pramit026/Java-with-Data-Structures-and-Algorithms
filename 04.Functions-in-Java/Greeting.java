08748c4e71b7a61dd176881d7d973393958b86e3
Pramit Sahoo <pramitsahoo.gnipst@gmail.com>
2022-03-28 08:29:56 +0530

    Add files via upload

---
+++ 04.Functions-in-Java/Greeting.java
@@ -0,0 +1,12 @@
+package com.Pramit;
+
+public class Greeting {
+    public static void main(String[] args) {
+      greeting();
+
+    }
+
+    static void greeting(){
+        System.out.println("Hello World!");
+    }
+}
